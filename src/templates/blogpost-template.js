4e49ff81ec753c1dee7da2acf5f327ab48ef7f55
kento <kento@toriwatari.bird>
2021-05-20 21:50:02 +0900

    Update blogpost-template.js
    前後の記事を表示

--- src/templates/blogpost-template.js
+++ src/templates/blogpost-template.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { graphql } from "gatsby"
+import { graphql, Link } from "gatsby"
 import Layout from "../components/layout"
 import Img from "gatsby-image"
 
@@ -35,7 +35,7 @@ const options = {
   }
 }
 
-const BlogPost = ({ data }) => {
+const BlogPost = ({ data, pageContext }) => {
   return (
     <Layout>
       <div className="eyecatch">
@@ -76,18 +76,22 @@ const BlogPost = ({ data }) => {
           </div>
 
           <ul className="postlink">
-            <li className="prev">
-              <a href="base-blogpost.html" rel="prev">
-              <FontAwesomeIcon icon={faChevronLeft} />
-                <span>前の記事</span>
-              </a>
-            </li>
-            <li className="next">
-              <a href="base-blogpost.html" rel="next">
-                <span>次の記事</span>
-                <FontAwesomeIcon icon={faChevronRight} />
-              </a>
-            </li>
+            {pageContext.next && (
+              <li className="prev">
+                <Link to={`/blog/post/${pageContext.next.slug}/`} rel="prev">
+                  <FontAwesomeIcon icon={faChevronLeft} />
+                  <span>{pageContext.next.title}</span>
+                </Link>
+              </li>
+            )}
+            {pageContext.previous && (
+              <li className="next">
+                <Link to={`/blog/post/${pageContext.previous.slug}/`} rel="next">
+                  <span>{pageContext.previous.title}</span>
+                  <FontAwesomeIcon icon={faChevronRight} />
+                </Link>
+              </li>
+              )}
           </ul>
 
         </div>
