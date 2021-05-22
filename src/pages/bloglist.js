cd6c216fa6425e45da4faa695db92afd123015fe
kento <kento@toriwatari.bird>
2021-05-22 21:20:10 +0900

    Create bloglist.js

---
+++ src/pages/bloglist.js
@@ -0,0 +1,71 @@
+import React from "react"
+import { graphql, Link } from "gatsby"
+import Img from "gatsby-image"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const BlogList = ({data, location}) => (
+  <Layout>
+
+    <SEO 
+      pagetitle="ブログ"
+      pagedesc="ブログです。"
+      pagepath={location.pathname}
+    />
+    <section className="content bloglist">
+      <div className="container">
+        <h1 className="bar">RECENT POSTS</h1>
+
+        <div className="posts">
+          {data.allContentfulBlogPost.edges.map(({ node }) => (
+            <article
+              className="post"
+              key={node.id}
+            >
+              <Link to={`/blog/post/${node.slug}`}>
+                <figure>
+                  <Img
+                    fluid={node.eyecatch.fluid}
+                    alt={node.eyecatch.description}
+                    style={{ height: "100%" }}
+                  />
+                </figure>
+                <h3>{ node.title }</h3>
+              </Link>
+            </article>
+          ))}
+        </div>
+      </div>
+    </section>
+  </Layout>
+)
+
+export const query = graphql`
+query {
+  allContentfulBlogPost(
+    sort: {
+      order: DESC,
+      fields: publishDate
+    }
+    skip: 0
+    limit: 6
+  ) {
+    edges {
+      node {
+        title
+        id
+        slug
+        eyecatch {
+          fluid(maxWidth: 500) {
+            ...GatsbyContentfulFluid_withWebp
+          }
+          description
+        }
+      }
+    }
+  }
+}
+`
+
+export default BlogList
