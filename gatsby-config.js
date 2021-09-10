265ef5e4acd04ceff71c2f015b0947d4f69574cb
kento <kento@toriwatari.bird>
2021-09-11 06:30:46 +0900

    🏃 #21 gatsby-plugin-typegenのインストール

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,6 +8,7 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-typescript`,
+    `gatsby-plugin-typegen`,
     `gatsby-plugin-sass`,
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-image`,
