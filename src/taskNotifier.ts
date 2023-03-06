d97eb1d07da72c2a6832ae070dbe6b8473df0e1d
kento <kento@toriwatari.bird>
2023-03-06 21:33:22 +0900

    ✨ Update

--- src/taskNotifier.ts
+++ src/taskNotifier.ts
@@ -1,8 +1,20 @@
+import { DynamoDB } from "aws-sdk"
 import { IncomingWebhook } from "@slack/webhook"
 
 export const handler = async () => {
+  const dynamodb = new DynamoDB({
+    region: "ap-northeast-1"
+  })
+
+  const result = await dynamodb.scan({
+    TableName: "tasks"
+  }).promise()
+
+  const tasks = result.Items.map((item) => {
+    return `✔ ${item.title.S}\n`
+  })
 
   const slackWebhook = new IncomingWebhook(process.env.SLACK_WEBHOOK_URL || "")
 
-  await slackWebhook.send("Hello TodoApp")
+  await slackWebhook.send(tasks.join(""))
 }
